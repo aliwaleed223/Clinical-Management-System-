b8d55ee477b92fd0a30b15deede54320260d2335
engbaqer <alkbaqer@gmail.com>
2025-05-05 00:05:33 +0300

    for test

--- back_end/server.js
+++ back_end/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 4000;
 // Connect to MongoDB
 // 'mongodb://localhost:27017'
 try {
-  await mongoose.connect("mongodb://localhost:27017/");
+  await mongoose.connect(process.env.DATABASE_URL);
 } catch (error) {
   handleError(error);
 }
